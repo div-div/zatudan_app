ae7f02fe2903109904b6ec1226fe8e1d2cabfa14
div-div <numatachiakii@gmail.com>
2020-09-08 17:56:37 +0900

    make the adminPage and manage

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,8 @@
-class User < ApplicationRecord
+class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :movies
+  has_many :users
   has_many :comments
-  belongs_to :admin
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+  belongs_to :admin
 end
